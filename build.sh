526f575937b44b4d32fd349f3ef78ffebe263d57
Andrew Laws <andrew.laws@iinet.net.au>
2019-06-13 10:09:14 +0800

    Fixed redirection to `/dev/null/` and missing template folder issues.

--- build.sh
+++ build.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
 # Runs the complete set of template combiners.
-# Necessary for WebGME to work properly.  
+# Necessary for WebGME to work properly.
 # Must be run when ever there has been a change to an *.ejs file.
-echo =================================================
+echo =======================================================
 echo Rebuilding templates.
-echo =================================================
-DIR=`pwd`
-cd src/plugins/C2Federates/Templates
-echo "Combining scripts in $(pwd)"
-node combine_templates.js > /dev/null 2>&1
-cd $DIR
-cd src/plugins/DeploymentExporter/Templates
-echo "Combining scripts in $(pwd)"
-node combine_templates.js > /dev/null 2>&1
-cd $DIR
-cd src/plugins/RunFederation/Templates
-echo "Combining scripts in $(pwd)"
-node combine_templates.js > /dev/null 2>&1
+echo =======================================================
+declare -a TEMPLATE_FOLDERS=(\
+    "src/plugins/C2Federates/Templates"\
+    "src/plugins/DeploymentExporter/Templates"\
+    "src/plugins/FederatesExporter/Templates"\
+    "src/plugins/RunFederation/Templates"\
+)
 
-echo =================================================
-echo Templates generated.
-echo =================================================
+CURRENT_DIR=`pwd`
+for template_folder in "${TEMPLATE_FOLDERS[@]}"
+do
+    cd $template_folder
+    echo "Combining scripts in $(pwd)"
+    node combine_templates.js
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - -"
+    cd $CURRENT_DIR
+done
 
-cd $DIR
+echo =======================================================
+echo ${#TEMPLATE_FOLDERS[@]} sets of templates rebuilt.
+echo =======================================================
+
+sleep 2
